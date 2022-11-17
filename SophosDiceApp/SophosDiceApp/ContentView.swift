7c94603b5413ee151fd22729580aa05c2209f110
Mauricio Rivero <rivero.mauricio@gmail.com>
2022-11-17 16:23:56 -0500

    updated the ui with a logo

--- SophosDiceApp/SophosDiceApp/ContentView.swift
+++ SophosDiceApp/SophosDiceApp/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
         VStack {
             //Image declaration
             Image("m-dice_\(number)")
+                .padding(.top, 150)
                 .onTapGesture {
                     //Here i added the functionality of random for the tap gesture on the image
                     number = Int.random(in: 1...6)
@@ -24,17 +25,29 @@ struct ContentView: View {
             
             //Button declaration
             Button(action: {
+                
             // Here i change the dice number with random
                 number = Int.random(in: 1...6)
             }){
-                Text("Rol Dice!")
+                //Text instance and graphis parameters
+                Text("Roll Dice!")
                     .fontWeight(.black)
                     .font(.system(size: 32))
-                    .foregroundColor(Color.purple)
+                    .foregroundColor(Color.white)
                     .padding()
+                    .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.roundedRectangle/*@END_MENU_TOKEN@*/)
+                    .border(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
             }
+            
+            //an image for a logo app
+            Image("logo-autor_1")
+                .padding(.top, 200)
+            Image("sophos-logo_1")
+            
         }
-        .padding()
+        
+        
     }
 }
 
